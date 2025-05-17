8b2ba342b30d10c374a22c358fbed45510383b43
Antooonio <tonperezalbano@gmail.com>
2025-05-17 19:48:46 +0200

    hi_github.py

--- hi_github.py
+++ hi_github.py
@@ -1,4 +1,6 @@
+import sys
 print ("Hi Github! I'm Antooonio.")
 lovely_user_name=input("Welcome, what's your name?")
 print("Thank you so much for giving it a try, "+lovely_user_name +", you are a wonderful person.")
 print("Please, consider sharing me your opinion!") 
+sys.exit()
