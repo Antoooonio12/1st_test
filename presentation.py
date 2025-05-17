1d70c3281dab1acadcbfd82d8506d4193bf2b4a3
Antooonio <tonperezalbano@gmail.com>
2025-05-17 19:56:28 +0200

    first presentation.

--- presentation.py
+++ presentation.py
@@ -2,5 +2,5 @@ import sys
 print ("Hi Github! I'm Antooonio.")
 lovely_user_name=input("Welcome, what's your name?")
 print("Thank you so much for giving it a try, "+lovely_user_name +", you are a wonderful person.")
-print("Please, consider sharing me your opinion!") 
+print("Please, consider sharing me your opinion!")
 sys.exit()
